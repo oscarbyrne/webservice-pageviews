6f962577df0bd93afd3739694f891abc80828676
Oscar Byrne <oscar.byrne@gmail.com>
2018-08-30 11:50:53 -0700

    implement migrations

--- pageviews/app.py
+++ pageviews/app.py
@@ -1,14 +1,21 @@
 from flask import Flask
 
-from .extensions import db
-from . import api
+from .extensions import (
+    db,
+    migrate,
+)
+from . import (
+    api,
+    commands,
+)
 
 
 def create_app():
     app = Flask(__name__)
-    db.init_app(app)
-    register_blueprints(app)
     set_config(app)
+    db.init_app(app)
+    migrate.init_app(app, db)
+    register_blueprints(app)
     return app
 
 def set_config(app):
