0f8f4510e216c82ae282a4983f6be2a5d6226d66
Oscar Byrne <oscar.byrne@gmail.com>
2018-09-03 15:46:51 -0700

    add progress bar for import script

--- pageviews/commands.py
+++ pageviews/commands.py
@@ -3,6 +3,7 @@ from datetime import datetime
 
 import click
 from flask.cli import with_appcontext
+from tqdm import tqdm
 
 from .extensions import db
 from .models import (
@@ -10,6 +11,20 @@ from .models import (
     User,
 )
 
+
+def count_lines_in_file(file):
+    """
+    From stackoverflow: https://stackoverflow.com/a/850962
+    """
+    lines = 0
+    buf_size = 1024 * 1024
+    read_f = file.read # loop optimization
+    buf = read_f(buf_size)
+    while buf:
+        lines += buf.count('\n')
+        buf = read_f(buf_size)
+    file.seek(0)
+    return lines
 
 @click.command()
 @click.argument('file', type=click.File('r'))
@@ -21,7 +36,10 @@ def add_visits_from_file(file):
         fieldnames=['datetime', 'user', 'os', 'device']
     )
     
-    for i, data in enumerate(reader):
+    enumerated = enumerate(reader)
+    length = count_lines_in_file(file)
+
+    for i, data in tqdm(enumerated, total=length):
 
         timestamp   = datetime.fromtimestamp(int(data['datetime']))
         user_id     = int(data['user'])
